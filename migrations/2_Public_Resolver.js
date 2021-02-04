cb995fce4c2f26b22316bac89117f2d772f8a1c2
sagostinelli <batonenbois@gmail.com>
2021-02-04 18:31:24 -0500

    ens storage address

--- migrations/2_Public_Resolver.js
+++ migrations/2_Public_Resolver.js
@@ -1,5 +1,5 @@
 var PublicResolver = artifacts.require("./PublicResolver.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(PublicResolver);
+  deployer.deploy(PublicResolver, "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e");
 };
